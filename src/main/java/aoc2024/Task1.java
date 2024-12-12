8935a075023095673cea390a4841466cafc6701d
Andrii Yeremeiev <andrii.yeremeiev@transferwise.com>
2024-12-12 19:30:29 +0000

    Adding task 1 and 2

---
+++ src/main/java/aoc2024/Task1.java
@@ -0,0 +1,71 @@
+package aoc2024;
+
+import aoc2024.common.Pair;
+import aoc2024.inputs.Task1Input;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task1 {
+
+    public static long getDistance() {
+        Pair<List<Integer>, List<Integer>> input = readInput();
+        input.getLeft().sort(Integer::compareTo);
+        input.getRight().sort(Integer::compareTo);
+
+        long result = 0;
+
+        for (int i = 0; i < input.getLeft().size(); i++) {
+            Integer leftValue = input.getLeft().get(i);
+            Integer rightValue = input.getRight().get(i);
+            result += Math.abs(leftValue - rightValue);
+        }
+
+        return result;
+    }
+
+    public static long getSimilarity(Pair<List<Integer>, List<Integer>> input) {
+        Map<Integer, Long> numberCounts = new HashMap<>();
+
+        List<Integer> rightList = input.getRight();
+
+        rightList.forEach(it -> {
+            if (numberCounts.containsKey(it)) {
+                numberCounts.put(it, numberCounts.get(it) + 1);
+            } else {
+                numberCounts.put(it, 1L);
+            }
+        });
+
+        long result = 0;
+        List<Integer> left = input.getLeft();
+
+        for (Integer it : left) {
+            if (numberCounts.containsKey(it)) {
+                result += it * numberCounts.get(it);
+            }
+        }
+
+        return result;
+    }
+
+    public static Pair<List<Integer>, List<Integer>> readInput() {
+        String input = Task1Input.INPUT_1;
+        String[] lines = input.split("\\n");
+
+        List<Integer> firstList = new ArrayList<>();
+        List<Integer> secondList = new ArrayList<>();
+        Arrays.stream(lines).map(it -> {
+            String[] lineValues = it.trim().split("\\s{2,}");
+            return new Pair<>(lineValues[0], lineValues[1]);
+        }).forEach(it -> {
+            firstList.add(Integer.valueOf(it.getLeft()));
+            secondList.add(Integer.valueOf(it.getRight()));
+        });
+
+        return Pair.of(firstList, secondList);
+    }
+
+}
---
+++ src/main/java/aoc2024/inputs/Task2Input.java
@@ -0,0 +1,13 @@
+package aoc2024.inputs;
+
+public class Task2Input {
+
+    public static String TASK_2 = """
+        1 2 7 4 5
+        7 6 4 2 1
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+            """;
+}
